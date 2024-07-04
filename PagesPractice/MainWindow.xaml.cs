75bd8163aa592a7c05c42a282d3e835a0ba2abd6
giolga <giolga.kumi@gmail.com>
2024-07-04 15:40:05 +0400

    Navigate Forward and Back a page

--- PagesPractice/MainWindow.xaml.cs
+++ PagesPractice/MainWindow.xaml.cs
@@ -23,13 +23,14 @@ namespace PagesPractice
     {
         public Page1 FirstPage;
         public Page2 SecondPage;
-
+        public Page3 ThirdPage;
         public MainWindow()
         {
             InitializeComponent();
 
             FirstPage = new Page1();
             SecondPage = new Page2();
+            ThirdPage = new Page3();
         }
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
@@ -39,6 +40,26 @@ namespace PagesPractice
         private void Button_Click_2(object sender, RoutedEventArgs e)
         {
             MainWindowFrame.Content = SecondPage;
+        }
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            MainWindowFrame.Content = ThirdPage;
+        }
+
+        private void Back_Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (MainWindowFrame.NavigationService.CanGoBack)
+            {
+                MainWindowFrame.NavigationService.GoBack();
+            }
+        }
+
+        private void Forward_Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (MainWindowFrame.NavigationService.CanGoForward)
+            {
+                MainWindowFrame.NavigationService.GoForward();
+            }
         }
     }
 }
